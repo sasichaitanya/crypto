122975f89c27ab056f2610f1f8f86be889849b92
chaitanya-gaian <geddada.chaitanya@gaiansolutions.com>
2021-05-23 19:17:09 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { ContentComponent } from '../app/content/content.component';
+const routes: Routes = [
+  { path: '', component: ContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
